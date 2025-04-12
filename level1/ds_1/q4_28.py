8f40b79bf137f177b0c93aa4597322020bc732d0
Baruh <baruhriss@gmail.com>
2025-04-12 13:54:28 +0300

    q4_28.py

--- level1/ds_1/q4_28.py
+++ level1/ds_1/q4_28.py
@@ -8,8 +8,33 @@
     
     
 #this function gets a list and uses selection sort
-def selection_sort_implementation(input = list):
-    
-    pass
-    
-    return sorted_array, number_of_basic_operations
+def selection_sort_implementation(_input: list[int]):
+    counter = 0
+    if type(_input) is not list:
+        raise TypeError("Invalid Input! needs to be a list")
+    counter += 1
+
+    for num in _input:
+        counter += 1  # for each assign
+        if not (isinstance(num, int) or isinstance(num, float)):
+            counter += 2
+            raise TypeError("Invalid type of at least one element in the list")
+        counter += 2
+
+    for i in range(len(_input)):
+        min_index = i
+        counter += 2
+        for j in range(i+1, len(_input)):
+            if _input[min_index] > _input[j]:
+                min_index = j
+                counter += 1
+            counter += 2
+
+        _input[i], _input[min_index] = _input[min_index], _input[i]
+        counter += 2
+
+    return _input, counter
+
+print(selection_sort_implementation([4,5,646,5,3,6,7,8,9]))
+
+
